61d94f63bdba8bd5822d7b8324bde23029cf4ad5
DOUARA Abderrahman <abderrahman@DOUARAs-MacBook-Air-2.local>
2018-12-08 16:01:51 +0100

    feat: Using Node, Webpack, and Babel workflow

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ShoppingCart from "./components/shoppingCart";
+import "./style.css"
+
+const shoppingCart = document.getElementById("shopping_cart");
+    ReactDOM.render(
+        <React.Fragment>
+            <ShoppingCart />
+        </React.Fragment>
+        , 
+        shoppingCart
+    );
+
