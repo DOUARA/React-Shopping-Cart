8cc526ae8474d9672c94de0fe672275a9108c1b5
DOUARA Abderrahman <abderrahman@DOUARAs-MacBook-Air-2.local>
2018-12-10 16:09:01 +0100

    feat: Add docker-compose file to facilitate deployment

---
+++ production_with_docker/build/precache-manifest.9e1de528533a476d36023b2951ccd1af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "da9915c1fa6e81461279513da2a5c81d",
+    "url": "/static/media/rose.da9915c1.jpg"
+  },
+  {
+    "revision": "08d6e9df67869111292153fb424cc7dd",
+    "url": "/static/media/cookie.08d6e9df.jpg"
+  },
+  {
+    "revision": "eb21a80f70f775ceeea0507d073eb79c",
+    "url": "/static/media/bg.eb21a80f.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ad28136eff3e6085fb0c",
+    "url": "/static/js/main.ad28136e.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "ad28136eff3e6085fb0c",
+    "url": "/static/css/main.89f295a7.chunk.css"
+  },
+  {
+    "revision": "c9717bd02ecc80f63f025ef4dbf2f604",
+    "url": "/index.html"
+  }
+];
